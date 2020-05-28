c9588a9ce2ffc60198fdf3ab7585361b89df7a73
Ben Parish <parishben13@gmail.com>
2020-05-28 00:48:43 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,20 +4,8 @@ class Anagram
   def initialize(word)
     @name=word
   end
- # def match(phrase)
-  #  matcher=[]
-   # words.each {|word|  matcher<< word if word == @pumpernickle}
-    #matcher
- #end
- def match(words)
-   getter=[]
-   words.select do |word| if @name.length == word.length
-   getter<< @name.match(/[word]/)
- end
- getter
-  end
- end
- #def match(array)
-  #  array.select {|x| x.split("").sort == @name.split("").sort}
-  #end   
+  
+ def match(array)
+    array.select {|word| word.split("").sort == @name.split("").sort}
+  end   
 end
