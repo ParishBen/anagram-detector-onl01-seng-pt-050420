e8cf4408009fe4c78dee160ca485437171e1e122
Ben Parish <parishben13@gmail.com>
2020-05-28 00:38:04 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :name
+  def initialize(word)
+    @name=word
+  end
+ # def match(phrase)
+  #  matcher=[]
+   # words.each {|word|  matcher<< word if word == @pumpernickle}
+    #matcher
+ #end
+ def match(words)
+   words.select do |word| @name(/[word]/)
+  end
+ end
+ #def match(array)
+  #  array.select {|x| x.split("").sort == @name.split("").sort}
+  #end   
+end
